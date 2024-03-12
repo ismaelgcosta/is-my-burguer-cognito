42c73db050083360246299987697856320d6953f
Ismael G Costa <notmymail@notmymail.com>
2024-03-12 00:16:33 -0300

    output do api gateway para utilização em outros repositórios

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,9 @@ output "cognito_id" {
   value       = aws_cognito_user_pool.is-my-burguer.id
 }
 
+output "api_gateway_domain" {
+  description = "The endpoint for the API Gateway domain"
+  value       = aws_apigatewayv2_api.cognito-api-gateway.id
+}
+
+
